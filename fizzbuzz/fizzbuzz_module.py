9a1dbf5a3d8389a67519cd7f1b91a27647659dc6
Ivanics Szilárd <ivanics.szilard@gmail.com>
2016-05-30 13:24:53 +0200

    FizzBuzz Ready

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,8 +1,12 @@
 def fizzbuzz(number):
-    return
+    return 'Fizz'*(not number % 3)+''*(not number % 15)+'Buzz'*(not number % 5) or number
 
 
 def main():
+    num = ""
+    for i in range(1, 101):
+        num = fizzbuzz(i)
+        print(num)
     return
 
 if __name__ == '__main__':

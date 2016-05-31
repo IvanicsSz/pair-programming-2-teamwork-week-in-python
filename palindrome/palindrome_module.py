a9d4cedce0fcb09a4bb4b08b62c75fd9a8978b30
Ivanics Szilárd <ivanics.szilard@gmail.com>
2016-05-31 09:21:02 +0200

    palindrome ready

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,9 +1,24 @@
 def palindrome(str):
-    return
+    str_forward = str.replace(" ",'')
+    str_backward = str[::-1].replace(" ",'')
+    result = ""
+    print(str_forward, str_backward)
+    #print("".join(str_forward), "".join(str_backward))
+    if str_forward.lower() == str_backward.lower():
+        print(str_forward, str_backward)
+        #if str_forward == str_backward:
+        result = True
+    else:
+        result = False
+    return result
 
 
 def main():
-    return
+    pali = input("Please write a string: ")
+    if palindrome(pali):
+        return print("Palindrome")
+    else:
+        return print("Not Palindrome")
 
 
 if __name__ == '__main__':

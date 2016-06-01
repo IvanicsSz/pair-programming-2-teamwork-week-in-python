a777ec77fd7877cf34a6a4482a3a2bdfe6d419d9
Ivanics Szilárd <ivanics.szilard@gmail.com>
2016-06-01 09:11:39 +0200

    simplier code

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,16 +1,9 @@
 def palindrome(str):
-    str_forward = str.replace(" ",'')
-    str_backward = str[::-1].replace(" ",'')
-    result = ""
-    print(str_forward, str_backward)
-    #print("".join(str_forward), "".join(str_backward))
+    str_forward = str.replace(" ", '')
+    str_backward = str[::-1].replace(" ", '')
     if str_forward.lower() == str_backward.lower():
-        print(str_forward, str_backward)
-        #if str_forward == str_backward:
-        result = True
-    else:
-        result = False
-    return result
+        return True
+    return False
 
 
 def main():

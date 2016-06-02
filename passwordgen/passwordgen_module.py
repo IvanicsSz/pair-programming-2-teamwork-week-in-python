289b8d231cb3535ead649f8d9087731cf6f63905
Ivanics Szilárd <ivanics.szilard@gmail.com>
2016-06-02 09:17:44 +0200

    pasword is ready

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,8 +1,22 @@
+import random
+
+
 def passwordgen():
-    return
+    box = [
+        [97, 122],
+        [65, 90],
+        [48, 57],
+        [35, 41],
+    ]
+    let = [chr(random.randint(box[i][0], box[i][1])) for i in range(4)]
+    let2 = [chr(random.randint(box[i][0], box[i][1])) for i in range(4)]
+    print("".join(let)+"".join(let2))
+    return "".join(let)+"".join(let2)
 
 
 def main():
+
+    passwordgen()
     return
 
 
